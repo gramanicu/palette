0efd824d2f1d67ad16eee22fb6635f327188ba04
Grama Nicolae <nicolae.grama@stud.acs.upb.ro>
2019-01-23 16:38:46 +0200

    Serving html

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => { 
+    response.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(port, (err) => {
